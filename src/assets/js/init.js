e934d670ff42f18605bc055217bf68739ab0cb37
Ismael <kismaelkemal@gmail.com>
2019-02-04 23:53:35 +0300

    Added "Explore" dropdown on the main navbar

--- src/assets/js/init.js
+++ src/assets/js/init.js
@@ -1,8 +1,10 @@
 // Initialize mobile navbar collapse
 $(document).ready(function () {
     $('.sidenav').sidenav();
+    $('.dropdown-trigger').dropdown();
     $('.tabs').tabs();
     $('.tabs .indicator').css({
       'background-color': '#4caf50'
-    })
+    });
+    
 });
