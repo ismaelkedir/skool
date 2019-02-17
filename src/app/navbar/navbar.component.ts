90e772a94d0aba04c476e290c050ce2019145b77
Ismael <kismaelkemal@gmail.com>
2019-02-17 18:16:54 +0300

    Fixed build errors

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -8,7 +8,7 @@ import { Router } from '@angular/router';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor(private _router: Router) { }
+  constructor(public _router: Router) { }
 
   ngOnInit() {
   }
